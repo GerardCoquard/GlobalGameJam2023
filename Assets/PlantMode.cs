23179de0017fb6dd57e2647c3794e0306349a565
HectorVergel <hvergel@edu.tecnocampus.cat>
2023-01-31 22:38:52 +0100

    cambio entre roots y pointer hacia donde crece v1

--- Assets/PlantMode.cs
+++ Assets/PlantMode.cs
@@ -10,15 +10,29 @@ public class PlantMode : MonoBehaviour
     public LayerMask layerMask;
     public Transform cam;
     bool growing;
+
+    public float interactPlantDistance;
+    public LayerMask interactPlantLayerMask;
+
+    public GameObject pointerPrefab;
+    GameObject currentPointer;
     private void Update() {
         if(InputManager.GetAction("Fire").WasPressedThisFrame())Grow();
         if(InputManager.GetAction("Fire").WasReleasedThisFrame())StopGrow();
         if(InputManager.GetAction("Aim").WasPressedThisFrame())Reset();
+        if (InputManager.GetAction("Interact").WasPressedThisFrame()) CheckIfControllerChanged();
     }
     void Grow()
     {
         RaycastHit hit;
         if(Physics.Raycast(cam.transform.position,cam.transform.forward,out hit, Mathf.Infinity,layerMask)) controller.StrartGrowing(hit.point);
+        if(currentPointer != null)
+        {
+            Destroy(currentPointer.gameObject);
+            currentPointer = null;
+        }
+        GameObject pointer = Instantiate(pointerPrefab, hit.point, Quaternion.identity);
+        currentPointer = pointer;
     }
     void StopGrow()
     {
@@ -27,5 +41,21 @@ public class PlantMode : MonoBehaviour
     void Reset()
     {
         controller.Reset();
+    }
+
+    void CheckIfControllerChanged()
+    {
+        Ray ray = new Ray(cam.transform.position,cam.transform.forward);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, interactPlantDistance, interactPlantLayerMask))
+        {
+            SetNewController(hit.collider.gameObject.GetComponentInParent<RootController>());
+        }
+    }
+
+    void SetNewController(RootController newController)
+    {
+        controller = newController;
     }
 }
