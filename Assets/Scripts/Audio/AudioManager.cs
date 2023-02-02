4454bb0686354c2e80c0cc88e46ec94c1f8aa381
HectorVergel <hvergel@edu.tecnocampus.cat>
2023-02-02 21:40:17 +0100

    v1_hector

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -8,6 +9,7 @@ public class AudioManager : MonoBehaviour
     public static AudioManager instance;
     public Dictionary<string, AudioClip> m_SoundsDictionary;
     private AudioSource m_MyAudioSource;
+    private AudioSource m_MyMusicSource;
 
     public AudioMixer m_MyAudioMixer;
     private void Awake()
@@ -45,8 +47,10 @@ public class AudioManager : MonoBehaviour
     public void PlaySound(string soundName, float volume)
     {
         if (m_SoundsDictionary.ContainsKey(soundName))
-        {
+        {   
+            
             m_MyAudioSource.volume = volume;
+            m_MyAudioSource.outputAudioMixerGroup = m_MyAudioMixer.FindMatchingGroups("SFX")[0];
             m_MyAudioSource.PlayOneShot(m_SoundsDictionary[soundName]);
         }
         else
@@ -55,6 +59,38 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    public void PlayMusic(string musicName, float volume)
+    {
+        if (m_SoundsDictionary.ContainsKey(musicName))
+        {
+            StartCoroutine(PlayMusicFadeOut());
+            m_MyMusicSource.volume = 0;
+            m_MyMusicSource.outputAudioMixerGroup = m_MyAudioMixer.FindMatchingGroups("Music")[0];
+            m_MyMusicSource.PlayOneShot(m_SoundsDictionary[musicName]);
+            StartCoroutine(PlayMusicFadeIn(volume));
+        }
+        else
+        {
+            Debug.LogWarning("Sound not found: " + musicName);
+        }
+    }
+
+    IEnumerator PlayMusicFadeIn(float maxVolume)
+    {
+        while(m_MyMusicSource.volume < maxVolume)
+        {
+            m_MyMusicSource.volume += 1 * Time.deltaTime;
+            yield return null;
+        }
+    }
+    IEnumerator PlayMusicFadeOut()
+    {
+        while(m_MyMusicSource.volume > 0)
+        {
+            m_MyMusicSource.volume -= 1 * Time.deltaTime;
+            yield return null;
+        }
+    }
 
     public void PlayAudioAtPosition(string soundName, Vector3 spawnPosition, float minDistance, float maxDistance)
     {
@@ -74,4 +110,5 @@ public class AudioManager : MonoBehaviour
             Debug.LogWarning("Sound not found: " + soundName);
         }
     }
+
 }
---
+++ Assets/Scripts/Audio/Button.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    public UnityEvent myEnterAction;
+    public UnityEvent myExitAction;
+
+    public void OnEnterAction()
+    {
+        myEnterAction.Invoke();
+    }
+
+    public void OnExitAction()
+    {
+        myExitAction.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnEnterAction();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        OnExitAction();
+    }
+}
